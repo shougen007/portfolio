33780b104471106a7d45e8825db44f848800d50d
sanghyun <son@sanghyuns-Mac-mini.local>
2024-05-30 20:03:47 +0900

    solove to syntax error

--- pages/404.js
+++ pages/404.js
@@ -1,13 +1,12 @@
+import Link from 'next/link';
 export default function PageNotFound(){
    
     return(
         <div className="flex justify-center">
             <h1>404 Not found!</h1>
-
-                <p>
-                    <a href="/"> [Go back to Home] </a>
-                </p>
-
+            <Link href="/">
+                <a>[Go back to Home]</a>
+            </Link>
         </div>
 
     );
