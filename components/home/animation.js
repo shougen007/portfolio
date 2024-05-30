3dec31041682ad6e7cfadcdd430ca8af9634e301
sanghyun <son@sanghyuns-Mac-mini.local>
2024-05-30 22:14:45 +0900

    modified favicon

--- components/home/animation.js
+++ components/home/animation.js
@@ -9,7 +9,7 @@ export default function Animation() {
       loop
       animationData={lottieJson}
       play
-      style={{ width: `150%`, height: `150%` }}
+      style={{ width: `100%`, height: `100%` }}
     />
   )
 }
